8b5ff74759b10c05b30c33cb9ecd0d3b21912dcb
Anıl Kılıç <87697493+anilklc@users.noreply.github.com>
2023-04-15 23:21:57 +0300

    Add files via upload

---
+++ qal/src/application/LoginController.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleGroup;
+
+public class LoginController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button close_button;
+
+    @FXML
+    private ToggleGroup giris_tipi;
+
+    @FXML
+    void close_button_Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        
+
+    }
+
+}
---
+++ qal/src/application/Main.java
@@ -0,0 +1,36 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
+			Scene scene = new Scene(root,600,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.getIcons().add(new Image("D:\\javaFX\\qal\\src\\image\\icon.png"));
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
+
+
+			primaryStage.setTitle("Login");
+			
+			
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
