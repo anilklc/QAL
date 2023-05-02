17c1a7e8782c0e536df8c756347dadc37a33ea46
anilklc <hage4950@gmail.com>
2023-05-02 15:42:04 +0300

    Combobox and bug fixed

--- qal/src/application/StudentController.java
+++ qal/src/application/StudentController.java
@@ -63,6 +63,10 @@ public class StudentController {
     @FXML
     private Label uyaridel_label;
     
+    @FXML
+    private Label uyari3_label;
+    
+    
     
     DBHelper db=new DBHelper();
     PreparedStatement statement;
@@ -93,24 +97,76 @@ public class StudentController {
 		} 
     	
     }
+    
+    void studentAdd(){
+	   	try {
+			db.connectOpen();
+	    	query="INSERT INTO student(studentName, studentSurname, studentClass, studentUsername, studentPassword) VALUES (?,?,?,?,?)";
+	    	statement=db.connection.prepareStatement(query);
+	    	statement.setString(1,studentName_text.getText().trim());
+	    	statement.setString(2,studentSurname_text.getText().trim());
+	    	statement.setString(3,studentClass_text.getText().trim());
+	    	statement.setString(4,studentUsername_text.getText().trim());
+	    	statement.setString(5,studentPassword_text.getText().trim());
+	    	statement.execute();
+	    	db.connectClose();
+	        statement.close();
+	        comboBoxLoad(studentName_cBox, studentSurname_cBox);
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}  
+	   }
+	   
+	   void studentDelete(){
+	   	try {
+			db.connectOpen();
+	    	query="DELETE FROM student WHERE studentName=? AND studentSurname=? AND studentClass=?";
+	    	statement=db.connection.prepareStatement(query);
+	    	statement.setString(1,studentName_cBox.getSelectionModel().getSelectedItem());
+	    	statement.setString(2,studentSurname_cBox.getSelectionModel().getSelectedItem());
+	    	statement.setString(3,studentClassDel_text.getText().trim());
+	    	statement.execute();
+	    	db.connectClose();
+	        statement.close();
+	        comboBoxLoad(studentName_cBox, studentSurname_cBox);
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		} 
+		
+		   
+	   }
    
 
     @FXML
     void studentAdd_button_Click(ActionEvent event) {
-    	comboBoxLoad(studentName_cBox, studentSurname_cBox);
+    	if(studentName_text.getText()!="" & studentSurname_text.getText()!=""& studentUsername_text.getText()!="" & studentPassword_text.getText()!="" & studentClass_text.getText()!="") {
+    		studentAdd();
+            comboBoxLoad(studentName_cBox, studentSurname_cBox);
+    	}
+    	else {
+    		uyariAdd_label.setVisible(true);
+    		
+    	}
 
     }
 
     @FXML
     void studentDelete_button_Click(ActionEvent event) {
-    	comboBoxLoad(studentName_cBox, studentSurname_cBox);
-
+    	if(studentName_cBox.getSelectionModel().getSelectedItem()!=null & studentSurname_cBox.getSelectionModel().getSelectedItem()!=null & studentClassDel_text.getText()!="") {
+    		studentDelete();
+            comboBoxLoad(studentName_cBox, studentSurname_cBox);
+    	}
+    	else {
+    		uyaridel_label.setVisible(true);
+    		
+    	}
 
     }
 
     @FXML
     void initialize() {
     	comboBoxLoad(studentName_cBox, studentSurname_cBox);
+    	
     }
 
 }
