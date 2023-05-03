2a557acf083a22555a1d873c0a3cb133d70c2801
anilklc <hage4950@gmail.com>
2023-05-03 17:08:38 +0300

    Teacher Announcement Add-Delete Stage Desing

---
+++ qal/src/application/AnnouncementEditTeacherController.java
@@ -0,0 +1,111 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class AnnouncementEditTeacherController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button announcementAdd_button;
+
+    @FXML
+    private Button announcementDelete_button;
+
+    @FXML
+    private Button announcementGet_button;
+
+    @FXML
+    private Label announcementName_label;
+
+    @FXML
+    private TextField announcementName_text;
+
+    @FXML
+    private ComboBox<String> announcement_cBox;
+
+    @FXML
+    private Label announcement_label;
+
+    @FXML
+    private TextArea announcement_textarea;
+
+    @FXML
+    private Label info;
+
+    @FXML
+    private Label info1;
+
+    @FXML
+    private Label lesson_label;
+
+    @FXML
+    private ComboBox<String> lesson_text;
+
+    @FXML
+    private Label passwordAdd_label;
+
+    @FXML
+    private TextField passwordAdd_text;
+
+    @FXML
+    private TextField passwordDel_text;
+
+    @FXML
+    private Label password_label;
+
+    @FXML
+    private Label usernameAdd_label;
+
+    @FXML
+    private TextField usernameAdd_text;
+
+    @FXML
+    private TextField usernameDel_text;
+
+    @FXML
+    private Label username_label;
+
+    @FXML
+    private Label uyariAdd_label;
+
+    @FXML
+    private Label uyaridel_label;
+
+    @FXML
+    private Label uyaridel_label1;
+
+    @FXML
+    void announcementAdd_button_Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void announcementDelete_button_Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void announcementGet_button_Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        
+
+    }
+
+}
