69312436486af375aacb34ccfeeb8f496ab919c1
anilklc <hage4950@gmail.com>
2023-05-04 22:43:25 +0300

    Announcement Admin and Teacher Stage Fixed

--- qal/src/application/AnnouncementEditController.java
+++ qal/src/application/AnnouncementEditController.java
@@ -1,7 +1,10 @@
 package application;
 
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
+import DB.DBHelper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -54,11 +57,6 @@ public class AnnouncementEditController {
     @FXML
     private ComboBox<String> lesson_text;
 
-    @FXML
-    private Label passwordAdd_label;
-
-    @FXML
-    private TextField passwordAdd_text;
 
     @FXML
     private Label usernameAdd_label;
@@ -77,24 +75,144 @@ public class AnnouncementEditController {
 
     @FXML
     private Label uyaridel_label1;
+    
+    @FXML
+    private ComboBox<String> announcementDate_cBox;
+
+    @FXML
+    private Label announcementDate_label;
+    
+    DBHelper db=new DBHelper();
+    PreparedStatement statement;
+    ResultSet resultSet;
+    String query;
+    void comboBoxLoad(ComboBox<String> cmb,String query,String columnName) {
+    	
+    	try {	
+    		db.connectOpen();
+        	statement=db.connection.prepareStatement(query);
+        	resultSet=statement.executeQuery();
+        	while (resultSet.next())
+            { cmb.getItems().addAll(resultSet.getString(columnName)); 
+            }
+        	
+        	db.connectClose();
+            statement.close();
+            resultSet.close();
+       
+    		
+    	}catch (Exception e) {
+    		System.out.println(e.getMessage());
+		} 
+    	
+    }
+    
+      
+    
+    void announcementGet(){
+       	try {
+    		db.connectOpen();
+        	query="SELECT * FROM announcement WHERE announcementUsername=?";
+        	statement=db.connection.prepareStatement(query);
+        	statement.setString(1,username_cBox.getSelectionModel().getSelectedItem());
+        	resultSet=statement.executeQuery();
+        	announcement_cBox.getItems().clear();
+            announcementDate_cBox.getItems().clear();
+        	while (resultSet.next())
+            { announcement_cBox.getItems().addAll(resultSet.getString("announcementName"));
+              announcementDate_cBox.getItems().addAll(resultSet.getString("announcementDate"));
+            }
+        	db.connectClose();
+            statement.close();
+            resultSet.close();
+            
+    	}catch (Exception e) {
+    		System.out.println(e.getMessage());
+    	}  
+       }
+       
+       void announcementDelete(){
+       	try {
+    		db.connectOpen();
+        	query="DELETE FROM announcement WHERE announcementName=? and announcementDate=?;";
+        	statement=db.connection.prepareStatement(query);
+        	statement.setString(1,announcement_cBox.getSelectionModel().getSelectedItem());
+        	statement.setString(2,announcementDate_cBox.getSelectionModel().getSelectedItem());
+        	statement.execute();
+        	db.connectClose();
+            statement.close();
+            username_cBox.getItems().clear();
+            announcement_cBox.getItems().clear();
+            announcementDate_cBox.getItems().clear();
+            comboBoxLoad(username_cBox,"SELECT DISTINCT(announcementUsername) FROM announcement","announcementUsername");
+    	}catch (Exception e) {
+    		System.out.println(e.getMessage());
+    	} 
+    	
+    	   
+       }
+       
+       
+       void announcementAdd(){
+    	   	try {
+    			db.connectOpen();
+    	    	query="INSERT INTO announcement(announcementName, announcementText, announcementUsername, announcementClass,announcementDate) VALUES (?,?,?,?,(SELECT CURRENT_DATE))";
+    	    	statement=db.connection.prepareStatement(query);
+    	    	statement.setString(1,announcementName_text.getText().trim());
+    	    	statement.setString(2,announcement_textarea.getText());
+    	    	statement.setString(3,usernameAdd_text.getText().trim());
+    	    	statement.setString(4,lesson_text.getSelectionModel().getSelectedItem());
+    	    	statement.execute();
+    	    	db.connectClose();
+    	        statement.close();
+    	        username_cBox.getItems().clear();
+    	        comboBoxLoad(username_cBox,"SELECT DISTINCT(announcementUsername) FROM announcement","announcementUsername");
+    		}catch (Exception e) {
+    			System.out.println(e.getMessage());
+    		}  
+    	   }
+       
 
     @FXML
     void announcementAdd_button_Click(ActionEvent event) {
-
+    	
+    	if(announcementName_text.getText()!="" & announcement_textarea.getText()!="" & usernameAdd_text.getText()!="" & lesson_text.getSelectionModel().getSelectedItem()!=null) {
+    		announcementAdd();
+    	}
+    	else {
+    		uyariAdd_label.setVisible(true);
+    	}
     }
 
     @FXML
     void announcementDelete_button_Click(ActionEvent event) {
-
+    	if(announcement_cBox.getSelectionModel().getSelectedItem()!=null & announcementDate_cBox.getSelectionModel().getSelectedItem()!=null) {
+    		announcementDelete();
+    		announcementDelete_button.setDisable(true);
+    	}
+    	else {
+    		uyaridel_label.setVisible(true);
+    	}
     }
 
     @FXML
     void announcementGet_button_Click(ActionEvent event) {
-
+    	if(username_cBox.getSelectionModel().getSelectedItem()!=null) {
+    		announcementGet();
+    		announcementDelete_button.setDisable(false);
+    	}
+    	else {
+    		uyaridel_label.setVisible(true);
+    	}
+    	
     }
 
     @FXML
-    void initialize() {
+    void initialize(){
+    	
+        comboBoxLoad(lesson_text,"SELECT * FROM lesson","lessonName");
+        announcementDelete_button.setDisable(true);
+        comboBoxLoad(username_cBox,"SELECT DISTINCT(announcementUsername) FROM announcement","announcementUsername");
         
 
     }
--- qal/src/application/AnnouncementEditTeacherController.java
+++ qal/src/application/AnnouncementEditTeacherController.java
@@ -1,7 +1,11 @@
 package application;
 
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
+
+import DB.DBHelper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -34,7 +38,10 @@ public class AnnouncementEditTeacherController {
     private TextField announcementName_text;
 
     @FXML
-    private ComboBox<String> announcement_cBox;
+    private TextField passwordDel_text;
+    
+    @FXML
+    private ComboBox<String> announcementName_cBox;
 
     @FXML
     private Label announcement_label;
@@ -56,12 +63,6 @@ public class AnnouncementEditTeacherController {
 
     @FXML
     private Label passwordAdd_label;
-
-    @FXML
-    private TextField passwordAdd_text;
-
-    @FXML
-    private TextField passwordDel_text;
 
     @FXML
     private Label password_label;
@@ -86,26 +87,158 @@ public class AnnouncementEditTeacherController {
 
     @FXML
     private Label uyaridel_label1;
+    
+    @FXML
+    private ComboBox<String> announcementDate_cBox;
 
     @FXML
-    void announcementAdd_button_Click(ActionEvent event) {
+    private Label announcementDate_label;
 
+    DBHelper db=new DBHelper();
+    PreparedStatement statement;
+    ResultSet resultSet;
+    String query;
+    void comboBoxLoad(ComboBox<String> cmb,String query,String columnName) {
+    	
+    	try {	
+    		db.connectOpen();
+        	statement=db.connection.prepareStatement(query);
+        	resultSet=statement.executeQuery();
+        	while (resultSet.next())
+            { cmb.getItems().addAll(resultSet.getString(columnName)); 
+            }
+        	
+        	db.connectClose();
+            statement.close();
+            resultSet.close();
+       
+    		
+    	}catch (Exception e) {
+    		System.out.println(e.getMessage());
+		} 
+    	
+    }
+    
+    
+    void announcementAdd(){
+	   	try {
+			db.connectOpen();
+	    	query="INSERT INTO announcement(announcementName, announcementText, announcementUsername, announcementClass,announcementDate) VALUES (?,?,?,?,(SELECT CURRENT_DATE))";
+	    	statement=db.connection.prepareStatement(query);
+	    	statement.setString(1,announcementName_text.getText().trim());
+	    	statement.setString(2,announcement_textarea.getText());
+	    	statement.setString(3,usernameAdd_text.getText().trim());
+	    	statement.setString(4,lesson_text.getSelectionModel().getSelectedItem());
+	    	statement.execute();
+	    	db.connectClose();
+	        statement.close();
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}  
+	   }
+    
+    
+    void announcementGet(){
+       	try {
+    		db.connectOpen();
+        	query="SELECT * FROM announcement WHERE announcementUsername=?";
+        	statement=db.connection.prepareStatement(query);
+        	statement.setString(1,usernameDel_text.getText().trim());
+        	resultSet=statement.executeQuery();
+            announcementDate_cBox.getItems().clear();
+        	while (resultSet.next())
+            { announcementName_cBox.getItems().addAll(resultSet.getString("announcementName"));
+              announcementDate_cBox.getItems().addAll(resultSet.getString("announcementDate"));
+            }
+        	db.connectClose();
+            statement.close();
+            resultSet.close();
+            
+    	}catch (Exception e) {
+    		System.out.println(e.getMessage());
+    	}  
+       }
+       
+       void announcementDelete(){
+       	try {
+    		db.connectOpen();
+        	query="DELETE FROM announcement WHERE announcementName=? and announcementDate=?;";
+        	statement=db.connection.prepareStatement(query);
+        	statement.setString(1,announcementName_cBox.getSelectionModel().getSelectedItem());
+        	statement.setString(2,announcementDate_cBox.getSelectionModel().getSelectedItem());
+        	statement.execute();
+        	db.connectClose();
+            statement.close();
+            announcementName_cBox.getItems().clear();
+            announcementDate_cBox.getItems().clear();
+    	}catch (Exception e) {
+    		System.out.println(e.getMessage());
+    	} 
+    	
+    	   
+       }
+    
+       
+       void userControl() {
+    	   
+    	   try {db.connectOpen();
+       	query="SELECT * FROM teacher where teacherUsername=? and teacherPassword=?";
+       	statement=db.connection.prepareStatement(query);
+       	statement.setString(1, usernameDel_text.getText().trim());
+       	statement.setString(2, passwordDel_text.getText().trim());
+       	resultSet=statement.executeQuery();
+       	resultSet.next();
+       	if (usernameDel_text.getText().equals(resultSet.getString("teacherUsername")) && passwordDel_text.getText().equals(resultSet.getString("teacherPassword")))
+           {announcementName_cBox.getItems().clear();
+            announcementDate_cBox.getItems().clear();
+       		announcementGet();
+           }
+       	
+       	db.connectClose();
+           statement.close();
+           resultSet.close();
+       	}catch (Exception e) {
+       		uyaridel_label.setVisible(true);
+       	}
+       }
+    
+    @FXML
+    void announcementAdd_button_Click(ActionEvent event) {
+    	if(announcementName_text.getText()!="" & announcement_textarea.getText()!="" & usernameAdd_text.getText()!="" & lesson_text.getSelectionModel().getSelectedItem()!=null) {
+    		announcementAdd();
+    	}
+    	else {
+    		uyariAdd_label.setVisible(true);
+    	}
     }
 
     @FXML
     void announcementDelete_button_Click(ActionEvent event) {
-
+    	if(announcementName_cBox.getSelectionModel().getSelectedItem()!=null & announcementDate_cBox.getSelectionModel().getSelectedItem()!=null) {
+    		announcementDelete();
+    		announcementDelete_button.setDisable(true);
+    	}
+    	else {
+    		uyaridel_label.setVisible(true);
+    	}
     }
 
     @FXML
     void announcementGet_button_Click(ActionEvent event) {
-
-    }
+    	
+    	if(usernameDel_text.getText()!="" & passwordDel_text.getText()!="") {
+    	userControl(); 
+    	announcementDelete_button.setDisable(false);}
+    	else {
+    		uyaridel_label.setVisible(true);
+    	}
+    	}
 
     @FXML
     void initialize() {
-        
-
+    	 comboBoxLoad(lesson_text,"SELECT * FROM lesson","lessonName");
+         announcementDelete_button.setDisable(true);
+         //comboBoxLoad(username_cBox,"SELECT DISTINCT(announcementUsername) FROM announcement","announcementUsername");
     }
 
 }
