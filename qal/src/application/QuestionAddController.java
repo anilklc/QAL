3554a508122d81a92372a785ec74230c525fbd3a
anilklc <hage4950@gmail.com>
2023-05-09 19:42:39 +0300

    Question Add Stage Desing

---
+++ qal/src/application/QuestionAddController.java
@@ -0,0 +1,112 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class QuestionAddController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label answer_label;
+
+    @FXML
+    private TextField answer_text;
+
+    @FXML
+    private Button control_button;
+
+    @FXML
+    private TextField examName__text;
+
+    @FXML
+    private Label examName_label;
+
+    @FXML
+    private Label image_label;
+
+    @FXML
+    private TextField image_text;
+
+    @FXML
+    private Label info;
+
+    @FXML
+    private Label info1;
+
+    @FXML
+    private Label info1_label;
+
+    @FXML
+    private Label info2_label;
+
+    @FXML
+    private Label info3_label;
+
+    @FXML
+    private Label info4_label;
+
+    @FXML
+    private Label info5_label;
+
+    @FXML
+    private Label info6_label;
+
+    @FXML
+    private ComboBox<String> lesson_cBox;
+
+    @FXML
+    private Label lesson_label;
+
+    @FXML
+    private Label password_label;
+
+    @FXML
+    private PasswordField password_text;
+
+    @FXML
+    private Button questionAdd_button;
+
+    @FXML
+    private Label totalQuestion_label;
+
+    @FXML
+    private TextField totalQuestion_text;
+
+    @FXML
+    private Label username_label;
+
+    @FXML
+    private TextField username_text;
+
+    @FXML
+    private Label uyariAdd_label;
+
+    @FXML
+    void control_button_Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void questionAdd_button_Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+       
+
+    }
+
+}
