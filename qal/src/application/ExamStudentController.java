a5cdeb426f007821adddb0ed73e5ecae376e6d41
anilklc <hage4950@gmail.com>
2023-05-11 20:27:38 +0300

    Exam Stage Desing

---
+++ qal/src/application/ExamStudentController.java
@@ -0,0 +1,122 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class ExamStudentController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private RadioButton all_rbutton;
+
+    @FXML
+    private RadioButton all_rbutton1;
+
+    @FXML
+    private RadioButton all_rbutton11;
+
+    @FXML
+    private Button examControl_button;
+
+    @FXML
+    private Button examGo_button;
+
+    @FXML
+    private ComboBox<String> exam_cBox;
+
+    @FXML
+    private Button finishExam_button;
+
+    @FXML
+    private Label info;
+
+    @FXML
+    private Label info1;
+
+    @FXML
+    private Label lesson_label;
+
+    @FXML
+    private Button nextQuestion_button;
+
+    @FXML
+    private Label password_label;
+
+    @FXML
+    private PasswordField password_text;
+
+    @FXML
+    private Button previousQuestion_button;
+
+    @FXML
+    private Label questionInfo_label;
+
+    @FXML
+    private ToggleGroup see_type;
+
+    @FXML
+    private ToggleGroup see_type1;
+
+    @FXML
+    private ToggleGroup see_type11;
+
+    @FXML
+    private RadioButton student_rbutton;
+
+    @FXML
+    private RadioButton teacher_rbutton;
+
+    @FXML
+    private Label username_label;
+
+    @FXML
+    private TextField username_text;
+
+    @FXML
+    private Label uyari_label;
+
+    @FXML
+    void examCntrol_button_Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void examGo_button_Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void finishExam_button_Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nextQuestion_button(ActionEvent event) {
+
+    }
+
+    @FXML
+    void previousQuestion_button_Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+      
+    }
+
+}
