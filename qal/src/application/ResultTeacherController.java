eac082b53dd727bb49a5bdb6e030a218f56f26c1
anilklc <hage4950@gmail.com>
2023-05-23 19:03:50 +0300

    Result Teacher Stage Source Code Written

--- qal/src/application/ResultTeacherController.java
+++ qal/src/application/ResultTeacherController.java
@@ -3,10 +3,13 @@ package application;
 import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import DB.DBHelper;
 import Models.tableViewResult;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -17,6 +20,8 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class ResultTeacherController {
@@ -46,7 +51,7 @@ public class ResultTeacherController {
     private Label info1;
 
     @FXML
-    private ComboBox<String> lesson_cBox;
+    private ComboBox<String> examName_cBox;
 
     @FXML
     private TableColumn<tableViewResult, String> loginDate_clm;
@@ -85,13 +90,16 @@ public class ResultTeacherController {
     private TextField username_text;
 
     @FXML
-    private TextField username_text1;
+    private TextField userSearch_text;
 
     @FXML
     private Label uyari_label;
 
     @FXML
     private TableColumn<tableViewResult, String> wrongQuestion_clm;
+    
+    @FXML
+    private TableColumn<tableViewResult, String> lessonName_clm;
     
 
     DBHelper db=new DBHelper();
@@ -100,18 +108,82 @@ public class ResultTeacherController {
     String query;
     
 
+       
+       String teacherLesson,teacherID;
+       void userInfo() {
+         	
+         	try {	
+         		db.connectOpen();
+         		query="SELECT * FROM teacher WHERE teacherUsername=? and teacherPassword=?";
+             	statement=db.connection.prepareStatement(query);
+            	statement.setString(1, username_text.getText().trim());
+            	statement.setString(2, password_text.getText().trim());
+            	resultSet=statement.executeQuery();
+            	resultSet.next();
+            	
+            	if (username_text.getText().equals(resultSet.getString("teacherUsername")) && password_text.getText().equals(resultSet.getString("teacherPassword")))
+                {  teacherID=resultSet.getString("teacherID");
+                   teacherLesson=resultSet.getString("teacherLessonID");
+                }
+            	db.connectClose();
+                statement.close();
+                resultSet.close();
+         	}catch (Exception e) {
+         		System.out.println(e.getMessage());
+     		} 
+         	
+         }
+       
+       
+       tableViewResult tbl=null;
+       ObservableList<tableViewResult> tblList=FXCollections.observableArrayList();
+       
+       private void refreshTable() {
+     	  try {
+     		    db.connectOpen();
+     	        statement=db.connection.prepareStatement(query);
+        		resultSet=statement.executeQuery();
+        		tblList.clear();
+              while(resultSet.next()) {
+             	 
+             	 tblList.add(new tableViewResult(resultSet.getString("studentName"),resultSet.getString("teacherName"),resultSet.getString("examName"),resultSet.getString("lessonName"),resultSet.getString("correctQuestion"),resultSet.getString("wrongQuestion"),resultSet.getString("point"),resultSet.getString("loginDate")));
+             	 tableview.setItems(tblList);
+              }
+              db.connectClose();
+              statement.close();
+              resultSet.close();	
+        		
+        		
+        	}catch (SQLException e) {
+     			System.out.println(e.getMessage());
+     		}
+
+         }
+        
+         private void loadData() {
+         	refreshTable();
+         	studentName_clm.setCellValueFactory(new PropertyValueFactory<>("studentName"));
+         	examName_clm.setCellValueFactory(new PropertyValueFactory<>("examName"));
+         	lessonName_clm.setCellValueFactory(new PropertyValueFactory<>("lessonName"));
+         	correctQuestion_clm.setCellValueFactory(new PropertyValueFactory<>("correctQuestion"));
+         	wrongQuestion_clm.setCellValueFactory(new PropertyValueFactory<>("wrongQuestion"));
+         	point_clm.setCellValueFactory(new PropertyValueFactory<>("point"));
+         	loginDate_clm.setCellValueFactory(new PropertyValueFactory<>("loginDate"));
+
+         	
+         }
+
        void comboBoxLoad(ComboBox<String> cmb) {
     	
     	try {	
     		db.connectOpen();
-        	query="";
+        	query="SELECT examName FROM result INNER JOIN exam ON result.examID=exam.examID INNER JOIN teacher ON teacher.teacherID=exam.teacherID INNER JOIN student ON student.studentID=result.studentID INNER JOIN lesson ON lesson.lessonID=exam.lessonID WHERE teacher.teacherID="+teacherID;
         	statement=db.connection.prepareStatement(query);
         	resultSet=statement.executeQuery();
-        	lesson_cBox.getItems().clear();
+        	examName_cBox.getItems().clear();
         	
         	while (resultSet.next())
-            { cmb.getItems().addAll(resultSet.getString("lessonName")); 
-             
+            { cmb.getItems().addAll(resultSet.getString("examName")); 
             }
         	db.connectClose();
             statement.close();
@@ -124,17 +196,69 @@ public class ResultTeacherController {
     	
     }
     
+       @FXML
+       void find_cBox_Click(MouseEvent event) {
+    	   if(find_cBox.getSelectionModel().getSelectedItem()!=null) {
+       		if(find_cBox.getSelectionModel().getSelectedIndex()==0) {
+       			start_date.setVisible(true);
+       			examName_cBox.setVisible(false);
+       			userSearch_text.setVisible(false);
+           	}
+       		else if(find_cBox.getSelectionModel().getSelectedIndex()==1) {
+       			userSearch_text.setVisible(true);
+       			start_date.setVisible(false);
+       			examName_cBox.setVisible(false);
+               	}
+       		else if(find_cBox.getSelectionModel().getSelectedIndex()==2) {
+       			examName_cBox.setVisible(true);
+       			userSearch_text.setVisible(false);
+       			start_date.setVisible(false);
+               	}
+           	}
+           	
+           
+       }
 
     @FXML
     void resultFind_button_Click(ActionEvent event) {
-
+    	if(find_cBox.getSelectionModel().getSelectedItem()!=null) {
+    		if(find_cBox.getSelectionModel().getSelectedIndex()==0) {
+    			
+        	query="SELECT studentName,studentSurname,teacherName,examName,lessonName,correctQuestion,wrongQuestion,point,loginDate FROM result INNER JOIN exam ON result.examID=exam.examID INNER JOIN teacher ON teacher.teacherID=exam.teacherID INNER JOIN student ON student.studentID=result.studentID INNER JOIN lesson ON lesson.lessonID=exam.lessonID WHERE loginDate='"+start_date.getValue().toString()+"'";
+        	loadData();}
+    		else if(find_cBox.getSelectionModel().getSelectedIndex()==1) {
+            	query="SELECT studentName,studentSurname,teacherName,examName,lessonName,correctQuestion,wrongQuestion,point,loginDate FROM result INNER JOIN exam ON result.examID=exam.examID INNER JOIN teacher ON teacher.teacherID=exam.teacherID INNER JOIN student ON student.studentID=result.studentID INNER JOIN lesson ON lesson.lessonID=exam.lessonID WHERE studentName='"+userSearch_text.getText().trim()+"'";
+            	loadData();}
+    		else if(find_cBox.getSelectionModel().getSelectedIndex()==2) {
+            	query="SELECT studentName,studentSurname,teacherName,examName,lessonName,correctQuestion,wrongQuestion,point,loginDate FROM result INNER JOIN exam ON result.examID=exam.examID INNER JOIN teacher ON teacher.teacherID=exam.teacherID INNER JOIN student ON student.studentID=result.studentID INNER JOIN lesson ON lesson.lessonID=exam.lessonID WHERE examName='"+examName_cBox.getSelectionModel().getSelectedItem().toString()+"'";
+            	loadData();}
+        	}
+        	
+        	else {
+        		 uyari_label.setVisible(true);
+        	}
     }
 
     @FXML
     void resultGet_button_Click(ActionEvent event) {
-
+    	if(username_text.getText().trim()!="" & password_text.getText().trim()!="") {
+    		userInfo();
+    	
+    	query="SELECT studentName,studentSurname,teacherName,examName,lessonName,correctQuestion,wrongQuestion,point,loginDate FROM result INNER JOIN exam ON result.examID=exam.examID INNER JOIN teacher ON teacher.teacherID=exam.teacherID INNER JOIN student ON student.studentID=result.studentID INNER JOIN lesson ON lesson.lessonID=exam.lessonID WHERE exam.teacherID="+teacherID;
+    	loadData();
+    	find_cBox.setDisable(false);
+    	find_cBox.getItems().clear();
+    	find_cBox.getItems().add("Sınav Giriş Tarihi");
+    	find_cBox.getItems().add("Öğrenci Adı");
+    	find_cBox.getItems().add("Sınav Adı");
+    	comboBoxLoad(examName_cBox);
+    	resultFind_button.setDisable(false);
+    	}
+    	
+    	else {
+   		 uyari_label.setVisible(true);
     }
-
+    }
     @FXML
     void initialize() {
         
